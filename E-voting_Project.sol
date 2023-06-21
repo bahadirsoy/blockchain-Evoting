e275df47e5a8d11350824f285088a5bb100868f1
Bahadır <oxikairos@hotmail.com>
2023-06-21 10:15:45 +0300

    proposal is added

--- E-voting_Project.sol
+++ E-voting_Project.sol
@@ -1,6 +1,15 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >= 0.4 .22 < 0.7 .0;
 
 contract VotingApp {
-   
-    
+    struct Participant {
+        uint weight;
+        bool voted;
+        address delegate;
+        uint vote;
+    }
+
+    struct Proposal {
+        bytes32 name;
+        uint voteCount;
+    }
 }
