472d87d03666f49b9e410e79724cfc36ed76b401
Ali Yarkin CANER <yarkincaner@gmail.com>
2023-06-21 10:06:23 +0300

    add participant struct

--- E-voting_Project.sol
+++ E-voting_Project.sol
@@ -1,6 +1,10 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract VotingApp {
-   
-    
+  struct Participant {
+      uint weight;
+      bool voted;
+      address delegate;
+      uint vote;
+  }
 }
